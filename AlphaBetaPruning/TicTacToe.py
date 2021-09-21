150783761264aa59a164a658591c9e3cd50e9397
ak566g <anki01.g@gmail.com>
2021-09-22 02:32:48 +0530

    simulated annealing updated

--- AlphaBetaPruning/TicTacToe.py
+++ AlphaBetaPruning/TicTacToe.py
@@ -111,36 +111,6 @@ def alpha_beta_pruning(state, depth, player, alpha, beta):
     return best
 
 
-"""
-# Define the minimax function
-def minimax(state, depth, player):
-    if player == COMP:
-        best = [-1, -1, -infinity]
-    else:
-        best = [-1, -1, +infinity]
-
-    if depth == 0 or game_over(state):
-        score = evaluate(state)
-        return [-1, -1, score]
-
-    for cell in empty_cells(state):
-        x, y = cell[0], cell[1]
-        state[x][y] = player
-        score = minimax(state, depth - 1, -player)
-        state[x][y] = 0
-        score[0], score[1] = x, y
-
-        if player == COMP:
-            if score[2] > best[2]:
-                best = score  # max value
-        else:
-            if score[2] < best[2]:
-                best = score  # min value
-                
-    return best
-"""
-
-
 #  Clears the console
 def clean():
     os_name = platform.system().lower()
